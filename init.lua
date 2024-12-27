b98fa136f9901003a5b9e0e6f73399490c9a0902
raghavagrawal52 <raghavagrawal.g@gmail.com>
2024-12-27 14:45:11 +0530

    initial setup

---
+++ init.lua
@@ -0,0 +1,9 @@
+print("advent of neovim")
+
+require("custom.lazy")
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":.lua<CR>")
